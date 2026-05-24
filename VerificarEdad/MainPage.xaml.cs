9cccd3a2433fa15fd11870b1a817a095ba805885
Isra <kevig600@gmail.com>
2026-05-24 14:05:19 -0600

    Agregar archivos de proyecto.

---
+++ VerificarEdad/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace VerificarEdad
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnContarClicked(object? sender, EventArgs e)
+        {
+            int edad = Convert.ToInt32(txtEdad.Text);
+
+            if(edad >= 18)
+            {
+                lblResultado.Text = "Es mayor de edad";
+                lblResultado.TextColor = Color.FromRgb(120, 200, 120);
+            }
+            else
+            {
+                lblResultado.Text = "Es menor de edad";
+                lblResultado.TextColor = Color.FromRgb(200, 120, 120);
+            }
+        }
+    }
+}
